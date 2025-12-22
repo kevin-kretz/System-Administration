5eb0001b6eaf0771dde66786359c10873c982a80
Kevin Kretz <kkretz17@gmail.com>
2025-12-22 10:32:10 -0500

    Add script to install multiple software using winget

---
+++ winget-software-install.ps1
@@ -0,0 +1,14 @@
+winget install --force `
+  TheBrowserCompany.Arc `
+  Discord.Discord `
+  Docker.DockerDesktop `
+  Git.Git `
+  Google.Chrome.EXE `
+  Microsoft.PowerToys `
+  Mozilla.Firefox `
+  "Riot Game valorant.live" `
+  Valve.Steam `
+  JetBrains.WebStorm `
+  qBittorrent `
+  Plex.PlexMediaServer `
+  Microsoft.VisualStudioCode
